020d7bc30fd5bdf7085e24c18accec2cfbedaead
lgfh98 <lgfigueroah@outlook.com>
2021-09-10 15:59:44 -0500

    componente Switch en el router y register creado

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "../Home";
 import { Login } from "@components/Login";
+import { Register } from "@components/register";
 
 const App = () => (
   <BrowserRouter>
-    <Route exact path="/" component={Home} />
-    <Route exact path="/login" component={Login} />
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/register" component={Register} />
+    </Switch>
   </BrowserRouter>
 );
 
